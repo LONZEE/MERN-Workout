7a3ddccdfd94bd36730dcbd4296a4e451c2922ca
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-09-18 11:58:50 -0500

    dependencies

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const res = await fetch('http://localhost:4000/api/workouts');
+            const data = await res.json();
+            
+            if (res.ok) {
+                setWorkouts(data);
+            }
+        }; 
+        fetchWorkouts();   
+    }, []);
+
+
     return (
         <div className="home">
-        <h1>Home</h1>
+        <div className="workouts">
+            {workouts && workouts.map((workouts) => (
+                <p key={workouts.id}>{workouts.name}</p>
+            ))}
+            </div>
         </div>
     );
     };
