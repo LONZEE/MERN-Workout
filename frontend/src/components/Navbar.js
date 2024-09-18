abb4bf2d4ce360d02ea65e4b4a1c667c385b2d94
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-09-18 11:39:26 -0500

    added some styles

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import logo from '../images/pump.png';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="navbar">
+                <Link to="/" className="nav-logo">
+                <img src={logo} alt="PUMP Nation Logo" className="nav-logo-img" />
+                </Link>
+                <div className="nav-menu">
+                    <Link to="/workouts" className="nav-item">Workouts</Link>
+                    <Link to="/cardio" className="nav-item">Cardio</Link>
+                    <Link to="/contact" className="nav-item">Contact</Link>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
