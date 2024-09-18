5912ac6b09903425a1c62ec9c0d162af011983f3
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-09-18 12:29:06 -0500

    update

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,6 +1,6 @@
 const WorkoutDetails = ({ workouts }) => {
     return (
-        <div className="workout">
+        <div className="workout-details">
             <h2>{workouts.name}</h2>
             <p>{workouts.description}</p>
             <p><strong>Duration: </strong>{workouts.duration}</p>
